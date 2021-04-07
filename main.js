dba862062111a96f4bd6dfd74465b03679904634
Guilherme <guilhermeginezsilva@gmail.com>
2021-04-07 16:41:30 -0300

    last improvements

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(".title").on('click', function(event){
     let descriptions = $(event.target.parentElement).find( ".description" );
     let showing = descriptions.filter(function (index) {
-        return this.style.display == "block";
+        return this.style.display === "block";
     }).length > 0;
 
     $(document).find( ".project .description" ).css( "display", "none" );
-    
+
     if(!showing) {
-        $(event.target.parentElement).find( ".description" ).css( "display", "block" );
+        $(event.target.parentElement).find(".description" ).css( "display", "block" );
     }
 });
